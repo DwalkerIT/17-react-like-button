fbd638d2efde9aafa57b112bc4b98ebd4e1fec43
DWalkerIT <DWalkerIT@gmail.com>
2021-02-19 15:36:40 -0500

    finished like button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   let [num, setNum] = useState(0);
-  let [str, setStr] = useState("How much do you LIKE this button");
+  let [str, setStr] = useState("likes");
   function handleNumClick() {
     setNum(num + 1);
   }
-
+  useEffect(() => {
+    // Update the document title using the browser API
+    num === 1 ? setStr("like") : setStr("likes");
+  });
   return (
     <div>
-      <h1>{str}</h1>
-      <h1>{num}</h1>
+      {/* <h1>{str}</h1>
+      <h1>{num}</h1> */}
 
-      <button onClick={handleNumClick}>likes</button>
-      <button onClick={() => setStr("Like Tom Brady Loves Winning")}>
-        I LOVE IT
+      <button className="primaryBtn" onClick={handleNumClick}>
+        {num} {str}
       </button>
+      {/* <button className="primaryBtn" onClick={() => setStr("Like Tom Brady Loves Winning")}>
+        I LOVE IT
+      </button> */}
     </div>
   );
 }
